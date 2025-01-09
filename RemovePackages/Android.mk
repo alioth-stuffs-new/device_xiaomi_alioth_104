8acc642784f018ca41471b5cb2dc617c77334614
Asif <49162687+nhAsif@users.noreply.github.com>
2025-01-09 21:31:53 +0600

    Alioth: remove chrome from debloat list

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,8 +5,6 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    Chrome \
-    Chrome-Stub \
     Drive \
     GoogleCamera \
     Maps \
